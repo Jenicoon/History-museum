87d86c78305688229a4ec1d7399bbd5b747ca862
Jenicoon <dldnd123@gmail.com>
2025-03-24 13:15:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,21 +9,3 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-document.getElementById("toggleButton").addEventListener("click", function () {
-    let password = prompt("암호를 입력하세요:");
-    let correctPassword = "539";  // 설정할 암호
-
-    if (password === correctPassword) {
-        let content = document.getElementById("toggleContent");
-        if (content.style.display === "none" || content.style.display === "") {
-            content.style.display = "block";
-        } else {
-            content.style.display = "none";
-        }
-    } else {
-        alert("암호가 틀렸습니다!");
-    }
-});
-
-// 기본적으로 내용 숨기기
-document.getElementById("toggleContent").style.display = "none";
